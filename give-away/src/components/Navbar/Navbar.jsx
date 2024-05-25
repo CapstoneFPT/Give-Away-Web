ea9aaa0446d0c14c54ce0301077e550353fe3bb7
MinhTri <hminhtri03@gmail.com>
2024-05-25 15:01:45 +0700

    add-pages

--- give-away/src/components/Navbar/Navbar.jsx
+++ give-away/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
     const[menu,setMenu] = useState('shop');
   return (
@@ -11,14 +12,14 @@ const Navbar = () => {
             <p>Give Away</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu('shop')}}>Shop{menu==='shop'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('men')}}>Men{menu==='men'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('women')}}>Women{menu==='women'?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu('kids')}}>Kids{menu==='kids'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('shop')}}><Link to='/' className='no-underline'>Shop</Link>{menu==='shop'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('men')}}><Link to='/men' className='no-underline'>Men</Link>{menu==='men'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('women')}}><Link to='/women' className='no-underline'>Women</Link>{menu==='women'?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('kids')}}><Link to='/kids' className='no-underline'>Kids</Link>{menu==='kids'?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt=""/>
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt=""/></Link>
             <div className='nav-cart-count'>0</div>
         </div>
     </div>
