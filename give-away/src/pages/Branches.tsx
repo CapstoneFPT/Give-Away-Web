17efb7647ea8c3c137589229fc8cd1db92e25e2b
MinhTri <hminhtri03@gmail.com>
2024-07-05 11:03:23 +0700

    finishingBranches

--- give-away/src/pages/Branches.tsx
+++ give-away/src/pages/Branches.tsx
@@ -12,6 +12,7 @@ import {
   Spin,
 } from "antd";
 import DisplayItem from "../components/ItemsDisplay/ItemsDisplay";
+
 interface FashionItem {
   itemId: string;
   type: string;
@@ -47,31 +48,59 @@ const Branches: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [shops, setShops] = useState<ShopData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [categories, setCategories] = useState<any[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const categoryAPI = await fetch(
-          "http://103.177.111.181:8080/api/categories"
-        );
-        const fashionItemAPI = await fetch(
-          "http://103.177.111.181:8080/api/fashionitems?Status=Available"
-        );
-        const shopAPI = await fetch("http://103.177.111.181:8080/api/shops");
-        const category = await categoryAPI.json();
-        const fashionItemData = await fashionItemAPI.json();
-        const shop = await shopAPI.json();
+        const [categoryResponse, fashionItemResponse, shopResponse] =
+          await Promise.all([
+            fetch("http://giveawayproject.jettonetto.org:8080/api/categories"),
+            fetch(
+              "http://giveawayproject.jettonetto.org:8080/api/fashionitems?Status=Available&Type=Itembase&Type=ConsignedForSale"
+            ),
+            fetch("http://giveawayproject.jettonetto.org:8080/api/shops"),
+          ]);
 
+        if (
+          !categoryResponse.ok ||
+          !fashionItemResponse.ok ||
+          !shopResponse.ok
+        ) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const categoriesData = await categoryResponse.json();
+        categoriesData.data = await Promise.all(
+          categoriesData.data.map(async (cate: any) => {
+            return {
+              ...cate,
+              subCate: await (async function () {
+                const subCateRes = await fetch(
+                  `http://giveawayproject.jettonetto.org:8080/api/categories/${cate.categoryId}`
+                );
+                const subCate = await subCateRes.json();
+
+                return subCate.data;
+              })(),
+            };
+          })
+        );
+
+        const fashionItemData = await fashionItemResponse.json();
+        const shopData = await shopResponse.json();
+        setCategories(categoriesData.data);
+        setShops(shopData.data);
         setTotalCount(fashionItemData.data.totalCount);
-        setFashionItems(fashionItemData.data.items); // Updated to setFashionItems with data.data.items
-        console.log(shop);
-        setShops(shop.data);
-        console.log(category);
+        setFashionItems(fashionItemData.data.items);
         setIsLoading(false);
+        console.log(categoriesData);
+        console.log(branches);
       } catch (error) {
-        console.error("Failed to fetch items:", error);
+        console.error("Failed to fetch data:", error);
         setIsLoading(false);
       }
     };
+
     fetchData();
   }, []);
 
@@ -102,7 +131,7 @@ const Branches: React.FC = () => {
     const filter = async () => {
       try {
         const response = await fetch(
-          `http://103.177.111.181:8080/api/fashionitems?Status=Available&ShopId=${key}`
+          `http://giveawayproject.jettonetto.org:8080/api/fashionitems?Status=Available&Type=Itembase&Type=ConsignedForSale&ShopId=${key}`
         );
         const data = await response.json();
 
@@ -129,7 +158,7 @@ const Branches: React.FC = () => {
     const fetchItems = async () => {
       try {
         const response = await fetch(
-          `http://103.177.111.181:8080/api/fashionitems?Status=Available&pageNumber=${page}&pageSize=${pageSize}` +
+          `http://giveawayproject.jettonetto.org:8080/api/fashionitems?Status=Available&Type=Itembase&Type=ConsignedForSale&pageNumber=${page}&pageSize=${pageSize}` +
             stateApiLink
         );
         const data = await response.json();
@@ -154,151 +183,134 @@ const Branches: React.FC = () => {
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Header
-        style={{
-          padding: 0,
-          background: "#fff",
-          height: "100px",
-        }}
-      >
-        <Card
-          className="branches-header"
-          style={{
-            width: "100%",
-            backgroundColor: "black",
-            display: "flex",
-            justifyContent: "flex-start",
-            left: 0,
-          }}
-        >
-          <Space direction="horizontal" size={300}>
-            <Dropdown menu={{ items, onClick }} placement="bottom">
-              <Button
-                onClick={(e) => e.preventDefault()}
-                style={{ width: "400px" }}
-              >
-                {branches || "Choose a branch"}
-              </Button>
-            </Dropdown>
-
-            <Typography.Text style={{ color: "white", fontSize: "20px" }}>
-              {branches}
-            </Typography.Text>
-          </Space>
-        </Card>
-      </Header>
-      <Layout hasSider className="site-layout" style={{ width: "100%" }}>
-        <Sider
-          trigger={null}
-          width="15%"
-          style={{
-            overflow: "hidden",
-            height: "100vh",
-            display: "flex",
-            left: 0,
-            top: 0,
-            bottom: 0,
-            backgroundColor: "#fff",
-          }}
-        >
-          <div>
-            <h3>Men</h3>
-            <Menu
-              theme="light"
-              mode="vertical"
+      <Card>
+        <Card>
+          <Header
+            style={{
+              padding: 0,
+              background: "#fff",
+              height: "100px",
+            }}
+          >
+            <Card
+              className="branches-header"
               style={{
-                backgroundColor: "#fff",
                 width: "100%",
-              }}
-            >
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-            </Menu>
-          </div>
-          <div>
-            <h3>Women</h3>
-            <Menu
-              theme="light"
-              mode="vertical"
-              style={{
-                backgroundColor: "#fff",
-                overflow: "hidden",
-              }}
-            >
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-            </Menu>
-          </div>
-        </Sider>
-        <Content
-          style={{
-            overflow: "initial",
-            display: "relative",
-            width: "85%",
-            padding: 15,
-          }}
-        >
-          <Card className="item-display" style={{ maxHeight: "100vh" }}>
-            <Typography.Title
-              style={{
-                fontSize: 50,
-                justifyContent: "center",
+                backgroundColor: "black",
                 display: "flex",
-              }}
-              level={3}
-            >
-              Branch Inventory
-            </Typography.Title>
-            <div
-              style={{
-                marginTop: "20px",
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-                gap: "20px",
-                justifyContent: "center",
-                alignItems: "center",
-                maxHeight: "100vh",
-                height: "80vh",
-                border: "3px solid #f0f0f0",
+                justifyContent: "flex-start",
+                left: 0,
               }}
             >
-              {isLoading && (
-                <div
-                  style={{
-                    gridColumn: "4",
-                  }}
-                >
-                  <Spin size="large" />
-                </div>
-              )}
-              {!isLoading &&
-                fashionItems.map((item) => (
-                  <DisplayItem
-                    key={item.itemId}
-                    item={item}
-                    onAdd={() => handleAdd(item.itemId)}
-                  />
+              <Space direction="horizontal" size={300}>
+                <Dropdown menu={{ items, onClick }} placement="bottom">
+                  <Button
+                    onClick={(e) => e.preventDefault()}
+                    style={{ width: "400px" }}
+                  >
+                    {branches || "Choose a branch"}
+                  </Button>
+                </Dropdown>
+
+                <Typography.Text style={{ color: "white", fontSize: "20px" }}>
+                  {branches}
+                </Typography.Text>
+              </Space>
+            </Card>
+          </Header>
+        </Card>
+        <Layout hasSider className="site-layout" style={{ width: "100%" }}>
+          <Sider
+            trigger={null}
+            width="15%"
+            style={{
+              overflow: "hidden",
+              height: "100vh",
+              display: "flex",
+              left: 0,
+              top: 0,
+              bottom: 0,
+              backgroundColor: "#fff",
+            }}
+          >
+            {/* <Menu mode="inline">
+              <Menu.SubMenu key="categories" title="Categories">
+                {categories.map((category) => (
+                  <Menu.SubMenu
+                    key={category.categoryId}
+                    title={category.name}
+                  >
+                    {category.subCate.map(() => (
+                      <Menu.Item key={categoryId}>{sub.name}</Menu.Item>
+                    ))}
+                  </Menu.SubMenu>
                 ))}
-            </div>
-            <Pagination
-              onChange={pageChange}
-              current={currentPage}
-              style={{ justifyContent: "center", display: "flex" }}
-              defaultCurrent={1}
-              total={totalCount}
-              showSizeChanger={false}
-            />
-          </Card>
-        </Content>
-      </Layout>
+              </Menu.SubMenu>
+            </Menu>
+            </Menu> */}
+          </Sider>
+          <Content
+            style={{
+              overflow: "initial",
+              display: "relative",
+              width: "85%",
+              padding: 15,
+            }}
+          >
+            <Card className="item-display" style={{ maxHeight: "100vh" }}>
+              <Typography.Title
+                style={{
+                  fontSize: 50,
+                  justifyContent: "center",
+                  display: "flex",
+                }}
+                level={3}
+              >
+                Branch Inventory
+              </Typography.Title>
+              <div
+                style={{
+                  marginTop: "20px",
+                  display: "grid",
+                  gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+                  gap: "20px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  maxHeight: "100vh",
+                  height: "80vh",
+                  border: "3px solid #f0f0f0",
+                }}
+              >
+                {isLoading && (
+                  <div
+                    style={{
+                      gridColumn: "4",
+                    }}
+                  >
+                    <Spin size="large" />
+                  </div>
+                )}
+                {!isLoading &&
+                  fashionItems.map((item) => (
+                    <DisplayItem
+                      key={item.itemId}
+                      item={item}
+                      onAdd={() => handleAdd(item.itemId)}
+                    />
+                  ))}
+              </div>
+              <Pagination
+                onChange={pageChange}
+                current={currentPage}
+                style={{ justifyContent: "center", display: "flex" }}
+                defaultCurrent={1}
+                total={totalCount}
+                showSizeChanger={false}
+              />
+            </Card>
+          </Content>
+        </Layout>
+      </Card>
     </Layout>
   );
 };
