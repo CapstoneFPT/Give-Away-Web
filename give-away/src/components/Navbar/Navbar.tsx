e186082fd7ffb7712eca949937c49b5dda5b757b
MinhTri <hminhtri03@gmail.com>
2024-07-11 10:06:15 +0700

    fixShopContext

--- give-away/src/components/Navbar/Navbar.tsx
+++ give-away/src/components/Navbar/Navbar.tsx
@@ -4,18 +4,11 @@ import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import Login from "../../pages/Login";
-import { ShopContext } from "../../context/ShopContext";
+
 import SearchBar from "../SearchBar/SearchBar";
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-
-  const shopContext = useContext(ShopContext) ?? {};
-  const cartItems = shopContext;
   const handleSearch = () => {};
-  const totalItems = Object.values<number>(cartItems ?? {}).reduce(
-    (total: number, quantity: number) => total + quantity,
-    0
-  );
 
   return (
     <div className="container">
@@ -77,9 +70,8 @@ const Navbar = () => {
           <Login />
           <Link to="/cart" className="nav-cart">
             <img src={cart_icon} alt="" />
-            {totalItems > 0 && (
-              <div className="nav-cart-count">{totalItems}</div>
-            )}
+
+            <div className="nav-cart-count">TotalItem</div>
           </Link>
 
           <div className="wallet">225$</div>
