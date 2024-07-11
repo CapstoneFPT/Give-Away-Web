eb39e60fc25ffd4a7675754dc695e36cac44b36c
MinhTri <hminhtri03@gmail.com>
2024-07-11 10:06:15 +0700

    fixShopContext

--- give-away/src/index.tsx
+++ give-away/src/index.tsx
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import ShopContextProvider from "./context/ShopContext";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
