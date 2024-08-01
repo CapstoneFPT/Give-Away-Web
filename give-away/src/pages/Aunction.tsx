9ab3c2b9e0d00b373ac740b513381c5f030eee58
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-08-01 19:23:54 +0700

    fix auction

--- give-away/src/pages/Aunction.tsx
+++ give-away/src/pages/Aunction.tsx
@@ -2,8 +2,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Card, Row, Col, Button, Typography, List } from "antd";
 import Footer from "../components/Footer/Footer";
 import { useParams, useNavigate } from "react-router-dom";
-import { AuctionApi, AuctionItemDetailResponse, CreateBidRequest } from "../api";
-import Countdown from 'react-countdown';
+import {
+  AuctionApi,
+  AuctionItemDetailResponse,
+  CreateBidRequest,
+} from "../api";
+import Countdown from "react-countdown";
 import signalRService from "../pages/service/signalrService";
 import "../pages/Auction.css";
 import signalrService from "../pages/service/signalrService";
@@ -14,10 +18,14 @@ const Auction: React.FC = () => {
   const { auctionId } = useParams<{ auctionId: string }>();
   const navigate = useNavigate();
   const userId = JSON.parse(localStorage.getItem("userId") || "null");
-  const [product, setProduct] = useState<AuctionItemDetailResponse | null>(null);
+  const [product, setProduct] = useState<AuctionItemDetailResponse | null>(
+    null
+  );
   const [selectedImage, setSelectedImage] = useState<string>("");
   const [bids, setBids] = useState<CreateBidRequest[]>([]);
-  const [nextBidAmount, setNextBidAmount] = useState<number | undefined>(undefined);
+  const [nextBidAmount, setNextBidAmount] = useState<number | undefined>(
+    undefined
+  );
   const [remainingTime, setRemainingTime] = useState<number>(0);
   const [timerKey, setTimerKey] = useState<number>(0);
   const [auctionStarted, setAuctionStarted] = useState<boolean>(false);
@@ -27,57 +35,59 @@ const Auction: React.FC = () => {
     setNextBidAmount(newBid.amount);
   }, []);
 
- 
-    const fetchData = async () => {
-      try {
-        const auctionDetailApi = new AuctionApi();
-        const response = await auctionDetailApi.apiAuctionsIdGet(auctionId!);
-        console.log(response)
-        const fetchedProduct = response.data.auctionItem;
-        console.log(fetchedProduct)
+  const fetchData = async () => {
+    try {
+      const auctionDetailApi = new AuctionApi();
+      const response = await auctionDetailApi.apiAuctionsIdGet(auctionId!);
+      console.log(response);
+      const fetchedProduct = response.data.auctionItem;
+      console.log(fetchedProduct);
 
-        const productData: AuctionItemDetailResponse = {
-          initialPrice: fetchedProduct?.sellingPrice,
-          images: fetchedProduct?.images,
-          category: fetchedProduct?.category,
-          condition: fetchedProduct?.condition,
-          name: fetchedProduct?.name,
-          shop: {
-            address: fetchedProduct?.shop?.address,
-          },
-        };
+      const productData: AuctionItemDetailResponse = {
+        initialPrice: fetchedProduct?.sellingPrice,
+        images: fetchedProduct?.images,
+        category: fetchedProduct?.category,
+        condition: fetchedProduct?.condition,
+        name: fetchedProduct?.name,
+        shop: {
+          address: fetchedProduct?.shop?.address,
+        },
+      };
 
-        setProduct(productData);
-        setSelectedImage(fetchedProduct!.images![0]!.imageUrl!);
+      setProduct(productData);
+      setSelectedImage(fetchedProduct!.images![0]!.imageUrl!);
 
-        const initialBidAmount = fetchedProduct?.initialPrice || 0;
-        setNextBidAmount(initialBidAmount);
+      const initialBidAmount = fetchedProduct?.initialPrice || 0;
+      console.log(initialBidAmount);
+      setNextBidAmount(initialBidAmount);
 
-        const serverTimeApi = new AuctionApi();
-        const responseServerTime = await serverTimeApi.apiAuctionsCurrentTimeGet();
-        const currentTime = new Date(responseServerTime.data!);
-        const startTime = new Date(response.data.startDate!);
-        const endTime = new Date(response.data.endDate!);
+      const serverTimeApi = new AuctionApi();
+      const responseServerTime =
+        await serverTimeApi.apiAuctionsCurrentTimeGet();
+      const currentTime = new Date(responseServerTime.data!);
+      const startTime = new Date(response.data.startDate!);
+      const endTime = new Date(response.data.endDate!);
 
-        if (currentTime < startTime) {
-          setRemainingTime(Math.floor((startTime.getTime() - currentTime.getTime()) / 1000));
-          setAuctionStarted(false);
-        } else {
-          setRemainingTime(Math.floor((endTime.getTime() - currentTime.getTime()) / 1000));
-          setAuctionStarted(true);
-        }
-
-        setTimerKey((prev) => prev + 1);
-      } catch (error) {
-        console.error("Error fetching auction details:", error);
+      if (currentTime < startTime) {
+        setRemainingTime(
+          Math.floor((startTime.getTime() - currentTime.getTime()) / 1000)
+        );
+        setAuctionStarted(false);
+      } else {
+        setRemainingTime(
+          Math.floor((endTime.getTime() - currentTime.getTime()) / 1000)
+        );
+        setAuctionStarted(true);
       }
-    };
 
-
-    fetchData();
-
+      setTimerKey((prev) => prev + 1);
+    } catch (error) {
+      console.error("Error fetching auction details:", error);
+    }
+  };
 
   useEffect(() => {
+    fetchData();
     const setupSignalR = async () => {
       try {
         await signalRService.startConnection();
@@ -107,6 +117,8 @@ const Auction: React.FC = () => {
       memberId: userId,
       amount: nextBidAmount,
     };
+    console.log(auctionId);
+    console.log(bidRequest);
     try {
       await signalrService.placeBid(auctionId!, bidRequest);
     } catch (error) {
@@ -121,12 +133,13 @@ const Auction: React.FC = () => {
     const seconds = Math.floor((total % (1000 * 60)) / 1000);
     return (
       <div>
-        {hours}:{minutes.toString().padStart(2, "0")}:{seconds.toString().padStart(2, "0")}
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
       </div>
     );
   };
   const formatBalance = (balance: number) => {
-    return new Intl.NumberFormat('de-DE').format(balance);
+    return new Intl.NumberFormat("de-DE").format(balance);
   };
 
   if (!product) {
@@ -135,8 +148,8 @@ const Auction: React.FC = () => {
 
   return (
     <Card>
-      <h1 style={{ textAlign: 'center', fontSize: '40px' }}>Auction</h1>
-      <Row gutter={[16, 16]} style={{ margin: '10px' }}>
+      <h1 style={{ textAlign: "center", fontSize: "40px" }}>Auction</h1>
+      <Row gutter={[16, 16]} style={{ margin: "10px" }}>
         <Col span={4}>
           <Row gutter={[10, 8]}>
             {product.images?.map((image, index) => (
@@ -146,9 +159,12 @@ const Auction: React.FC = () => {
                   alt={`Thumbnail ${index}`}
                   style={{
                     width: "90%",
-                    height: '230px',
+                    height: "230px",
                     cursor: "pointer",
-                    border: selectedImage === image.imageUrl ? "2px solid #1890ff" : "none",
+                    border:
+                      selectedImage === image.imageUrl
+                        ? "2px solid #1890ff"
+                        : "none",
                   }}
                   onClick={() => setSelectedImage(image.imageUrl!)}
                 />
@@ -162,7 +178,7 @@ const Auction: React.FC = () => {
               src={selectedImage}
               alt={product.name!}
               className="zoom-image"
-              style={{ width: "90%", height: '750px' }}
+              style={{ width: "90%", height: "750px" }}
             />
           </div>
         </Col>
@@ -178,38 +194,40 @@ const Auction: React.FC = () => {
                   <strong>Condition:</strong> {product.condition}%
                 </Paragraph>
                 <Paragraph>
-                <strong>Size:</strong> {product.initialPrice}
-              </Paragraph>
+                  <strong>Size:</strong> {product.initialPrice}
+                </Paragraph>
               </Col>
               <Col span={10}>
-              <Paragraph>
-                <strong>Brand:</strong> {product.initialPrice}
-              </Paragraph>
-              <Paragraph>
-                <strong>Color:</strong> {product.initialPrice}
-              </Paragraph>
-              <Paragraph>
-                <strong>Gender:</strong> {product.initialPrice}
-              </Paragraph>
-              
+                <Paragraph>
+                  <strong>Brand:</strong> {product.initialPrice}
+                </Paragraph>
+                <Paragraph>
+                  <strong>Color:</strong> {product.initialPrice}
+                </Paragraph>
+                <Paragraph>
+                  <strong>Gender:</strong> {product.initialPrice}
+                </Paragraph>
               </Col>
               <Paragraph>
-                <strong style={{fontSize:'18px', color:'ThreeDLightShadow'}}>Initial Price: {formatBalance(product.initialPrice!)} VND</strong> 
+                <strong
+                  style={{ fontSize: "18px", color: "ThreeDLightShadow" }}
+                >
+                  Initial Price: {formatBalance(product.initialPrice!)} VND
+                </strong>
               </Paragraph>
-              
             </Row>
             <Paragraph>
-                <strong>Description:</strong> {product.note}
-              </Paragraph>
-            <Paragraph style={{ color: '#32b94b', fontSize: '20px' }}>
-              <strong>Current Bid: {nextBidAmount} VND </strong> 
+              <strong>Description:</strong> {product.note}
+            </Paragraph>
+            <Paragraph style={{ color: "#32b94b", fontSize: "20px" }}>
+              <strong>Current Bid: {nextBidAmount} VND </strong>
             </Paragraph>
           </Card>
-          <Card title="Bids History" style={{ marginTop: '10px' }}>
+          <Card title="Bids History" style={{ marginTop: "10px" }}>
             <List
-              style={{ maxHeight: '200px', overflowY: 'auto' }}
+              style={{ maxHeight: "200px", overflowY: "auto" }}
               dataSource={bids}
-              renderItem={bid => (
+              renderItem={(bid) => (
                 <List.Item>
                   <List.Item.Meta
                     title={` ${bid.amount} VND`}
@@ -218,11 +236,17 @@ const Auction: React.FC = () => {
                 </List.Item>
               )}
             />
-            <div style={{ marginTop: '10px' }}>
-              <p><strong>Next Bid Amount:  {nextBidAmount} VND </strong> </p>
+            <div style={{ marginTop: "10px" }}>
+              <p>
+                <strong>Next Bid Amount: {nextBidAmount} VND </strong>{" "}
+              </p>
               <Button
                 type="primary"
-                style={{ marginTop: '10px', backgroundColor: 'black', width: '100%' }}
+                style={{
+                  marginTop: "10px",
+                  backgroundColor: "black",
+                  width: "100%",
+                }}
                 onClick={handleBid}
               >
                 Place Bid
