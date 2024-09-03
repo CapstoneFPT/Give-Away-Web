cae887bc0a81c58bdd05c4b05eea46e2e053045e
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-09-03 11:48:11 +0700

    update order detail

--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -1,39 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { Card, Row, Col, Button, Spin, Tag, Image, Typography, Divider, Descriptions, Table } from "antd";
-import { useParams, useNavigate, useLocation, Link } from "react-router-dom";
+import React from "react";
+import {
+  Card,
+  Row,
+  Col,
+  Button,
+  Spin,
+  Tag,
+  Image,
+  Typography,
+  Divider,
+  Descriptions,
+  Table,
+  Space,
+} from "antd";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import NavProfile from "../components/NavProfile/NavProfile";
 import { FashionItemStatus, OrderApi, OrderLineItemListResponse } from "../api";
+import { useQuery } from "@tanstack/react-query";
+import { ColumnsType } from "antd/es/table";
 
-const { Title } = Typography;
+const { Title, Text, Paragraph } = Typography;
 
 const OrderDetail = () => {
   const { id } = useParams<{ id: string }>();
-  const [orderLineItems, setOrderLineItems] = useState<OrderLineItemListResponse[] | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
   const navigate = useNavigate();
-  const location = useLocation();
-  const { paymentMethod, recipientName, address, contactNumber, totalPrice, statusOder, createdDate } = location.state || {};
 
-  useEffect(() => {
-    const fetchOrderDetails = async () => {
-      setLoading(true);
-      try {
-        const orderApi = new OrderApi();
-        const response = await orderApi.apiOrdersOrderIdOrderlineitemsGet(id!);
-        setOrderLineItems(response.data?.items || []);
-        console.log(response);
-      } catch (error) {
-        console.error("Failed to fetch order details", error);
-        setOrderLineItems(null);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const orderApi = new OrderApi();
 
-    fetchOrderDetails();
-  }, [id]);
+  const { data: orderLineItems, isLoading: isOrderLineItemsLoading } = useQuery(
+    {
+      queryKey: ["orderLineItems", id],
+      queryFn: () => orderApi.apiOrdersOrderIdOrderlineitemsGet(id!),
+      select: (response) => response.data?.items || [],
+    }
+  );
 
-  if (loading) {
+  const { data: orderDetail, isLoading: isOrderDetailLoading } = useQuery({
+    queryKey: ["orderDetail", id],
+    queryFn: () => orderApi.apiOrdersOrderIdGet(id!),
+    select: (response) => response.data,
+  });
+
+  const isLoading = isOrderLineItemsLoading || isOrderDetailLoading;
+
+  if (isLoading) {
     return <Spin size="large" style={{ display: "block", margin: "auto" }} />;
   }
 
@@ -48,127 +58,250 @@ const OrderDetail = () => {
     return expirationDate > currentDate;
   };
 
-  const handleRefundClick = (orderLineItem: OrderLineItemListResponse, orderDetailId: string) => {
+  const handleRefundClick = (
+    orderLineItem: OrderLineItemListResponse,
+    orderDetailId: string
+  ) => {
     navigate("/refunds", { state: { items: [orderLineItem], orderDetailId } });
   };
-  const formatBalance = (balance:number) => {
-    return new Intl.NumberFormat('de-DE').format(balance);
+  const formatBalance = (balance: number) => {
+    return new Intl.NumberFormat("de-DE").format(balance);
   };
 
-
-  const columns = [
+  const columns: ColumnsType<OrderLineItemListResponse> = [
     {
-      title: 'Product',
-      dataIndex: 'itemImage',
-      key: 'itemImage',
-      render: (images: string[]) => (
-        <Image.PreviewGroup>
-          <Image src={images[0]} alt="Product Image" style={{ width: '100px', height: 'auto', borderRadius: '10px' }} />
-        </Image.PreviewGroup>
-      ),
-    },
-    {
-      title: 'Product Name',
-      dataIndex: 'itemName',
-      key: 'itemName',
-      render: (text: string, record: OrderLineItemListResponse) => (
+      title: "Product",
+      dataIndex: "itemImage",
+      align: "center",
+      key: "itemImage",
+      render: (images: string[], record: OrderLineItemListResponse) => (
         <>
-          {text}
-          <Tag color={record.itemStatus === FashionItemStatus.Sold ? 'green' : 'blue'} style={{ marginLeft: '10px' }}>
-            {record.itemStatus}
-          </Tag>
+          <Paragraph>
+            <Text
+              strong
+              style={{
+                fontSize: "16px",
+              }}
+            >
+              Name: {record.itemName}
+            </Text>
+          </Paragraph>
+          <Paragraph>
+            <Text>
+              <strong>{record.itemCode}</strong>
+            </Text>
+          </Paragraph>
+          <Image
+            src={images[0]}
+            alt="Product"
+            width={100}
+            height={100}
+            style={{ objectFit: "cover" }}
+          />
         </>
       ),
     },
-    {title: 'Item Code', dataIndex: 'itemCode', key: 'itemCode'},
-    { title: 'Unit Price', dataIndex: 'unitPrice', key: 'unitPrice', render: (price: number) => <strong>{formatBalance(price)} VND</strong> },
-    { title: 'Quantity', dataIndex: 'quantity', key: 'quantity' },
-    { title: 'Color', dataIndex: 'itemColor', key: 'itemColor' },
-    { title: 'Size', dataIndex: 'itemSize', key: 'itemSize' },
-    { title: 'Gender', dataIndex: 'itemGender', key: 'itemGender' },
-    { title: 'Brand', dataIndex: 'itemBrand', key: 'itemBrand' },
-    { title: 'Condition', dataIndex: 'condition', key: 'condition' },
-    { title: 'Shop', dataIndex: 'shopAddress', key: 'shopId' },
+    {
+      title: "Product Details",
+      dataIndex: "itemName",
+      key: "itemName",
+      align: "center",
+      render: (text: string, record: OrderLineItemListResponse) => (
+        <>
+          <Space direction="vertical" size={8} align="start">
+            <div>
+              <Typography>
+                <strong>Brand: </strong>{" "}
+                <Tag color="black">{record.itemBrand}</Tag>
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <strong>Size: </strong>
+                <Tag color="black">{record.itemSize}</Tag>
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <strong>Gender: </strong>
+                {record.itemGender}
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <strong>Color: </strong>
+                {record.itemColor}
+              </Typography>
+            </div>
+          </Space>
+        </>
+      ),
+    },
+    {
+      title: "Unit Price",
+      dataIndex: "unitPrice",
+      key: "unitPrice",
+      align: "center",
+      render: (price: number) => <strong>{formatBalance(price)} VND</strong>,
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+      width: 100,
+      align: "center",
+    },
+    { title: "Condition", dataIndex: "condition", key: "condition", align:"center" },
+    {
+      title: "Shop",
+      dataIndex: "shopAddress",
+      key: "shopId",
+      width: 200,
+      align: "center",
+    },
 
     {
-      title: 'Action',
-      key: 'action',
-      render: (text: any, record: OrderLineItemListResponse) => (
-        isRefundEligible(record.refundExpirationDate!) && record.itemStatus === FashionItemStatus.Refundable && (
+      title: "Action",
+      key: "action",
+      render: (text: any, record: OrderLineItemListResponse) =>
+        isRefundEligible(record.refundExpirationDate!) &&
+        record.itemStatus === FashionItemStatus.Refundable && (
           <Button
             type="primary"
-            style={{ backgroundColor: 'black', borderColor: 'black', width: '100px', height: '35px' }}
+            style={{
+              backgroundColor: "black",
+              borderColor: "black",
+              width: "100px",
+              height: "35px",
+            }}
             onClick={() => handleRefundClick(record, record.orderLineItemId!)}
           >
             Refund
           </Button>
-        )
-      ),
+        ),
     },
   ];
 
   return (
-    <Card style={{ borderRadius: '10px', boxShadow: '2px 2px 10px rgba(0,0,0,0.1)', padding: '20px' }}>
+    <Card
+      style={{
+        borderRadius: "10px",
+        boxShadow: "2px 2px 10px rgba(0,0,0,0.1)",
+        padding: "20px",
+      }}
+    >
       <Row gutter={[24, 24]}>
         <Col span={6}>
           <NavProfile />
         </Col>
         <Col span={18}>
-        <Card title={
-          <>
-            Order Detail
-            <Tag
-          color={
-            statusOder === "Completed"
-              ? "green"
-              : statusOder === "AwaitingPayment"
-              ? "yellow"
-              : statusOder === "OnDelivery"
-              ? "blue"
-              : "red"
-          }
-        >
-          {statusOder.toUpperCase()}
-        </Tag>
-          </>
-        }>
-        <Card bordered={false} style={{ borderRadius: '10px' }}>
-            <Title level={2} style={{ textAlign: 'center', marginBottom: '20px' }}>Order Detail</Title>
-            <Card title="Recipient Information"  bordered={false} style={{ borderRadius: '10px', marginBottom: '20px' }}>
-              <Descriptions column={1} bordered size="small">
-                
-                <Descriptions.Item label="Payment Method">{paymentMethod}</Descriptions.Item>
-                <Descriptions.Item label="Recipient Name">{recipientName}</Descriptions.Item>
-                <Descriptions.Item label="Address">{address}</Descriptions.Item>
-                <Descriptions.Item label="Phone Number">{contactNumber}</Descriptions.Item>
-                <Descriptions.Item label="Total"><strong>{formatBalance(totalPrice)} VND </strong></Descriptions.Item>
-                <Descriptions.Item label="Created At">{createdDate}</Descriptions.Item>
-
-              </Descriptions>
-            </Card>
-            <Table
-              columns={columns}
-              dataSource={orderLineItems}
-              rowKey="orderDetailId"
-              pagination={false}
-            />
-            <Divider />
-            <Link to="/order-list">
-              <Button
-                type="primary"
-                style={{
-                  backgroundColor: 'black',
-                  borderColor: 'black',
-                  width: '100px',
-                  height: '35px',
-                  marginTop: '20px',
-                }}
+          <Card
+            title={
+              <>
+                Order Detail
+                <Tag
+                  color={
+                    orderDetail?.status === "Completed"
+                      ? "green"
+                      : orderDetail?.status === "AwaitingPayment"
+                      ? "yellow"
+                      : orderDetail?.status === "OnDelivery"
+                      ? "blue"
+                      : "red"
+                  }
+                >
+                  {orderDetail?.status
+                    ? orderDetail?.status.toUpperCase()
+                    : "N/A"}
+                </Tag>
+              </>
+            }
+          >
+            <Card bordered={false} style={{ borderRadius: "10px" }}>
+              <Title
+                level={2}
+                style={{ textAlign: "center", marginBottom: "20px" }}
               >
-                Back
-              </Button>
-            </Link>
+                Order Detail
+              </Title>
+              <Card
+                title="Recipient Information"
+                bordered={false}
+                style={{ borderRadius: "10px", marginBottom: "20px" }}
+              >
+                <Descriptions column={1} bordered size="small">
+                  <Descriptions.Item label="Order Code">
+                    {orderDetail?.orderCode}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Payment Method">
+                    {orderDetail?.paymentMethod}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Recipient Name">
+                    {orderDetail?.reciepientName}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Customer Name">
+                    {orderDetail?.customerName}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Address">
+                    {orderDetail?.address}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Address Type">
+                    {orderDetail?.addressType}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Phone Number">
+                    {orderDetail?.phone}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Email">
+                    {orderDetail?.email}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Sub Total">
+                    {formatBalance(orderDetail?.subtotal || 0)} VND
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Shipping Fee">
+                    {formatBalance(orderDetail?.shippingFee || 0)} VND
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Discount">
+                    -{formatBalance(orderDetail?.discount || 0)} VND
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Total">
+                    <strong>
+                      {formatBalance(orderDetail?.totalPrice || 0)} VND{" "}
+                    </strong>
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Created At">
+                    {new Date(orderDetail?.createdDate || 0).toLocaleString()}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Payment Date">
+                    {new Date(orderDetail?.paymentDate || 0).toLocaleString()}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Completed Date">
+                    {new Date(orderDetail?.completedDate || 0).toLocaleString()}
+                  </Descriptions.Item>
+                </Descriptions>
+              </Card>
+              <Table
+                columns={columns}
+                dataSource={orderLineItems}
+                rowKey="orderDetailId"
+                pagination={false}
+              />
+              <Divider />
+              <Link to="/order-list">
+                <Button
+                  type="primary"
+                  style={{
+                    backgroundColor: "black",
+                    borderColor: "black",
+                    width: "100px",
+                    height: "35px",
+                    marginTop: "20px",
+                  }}
+                >
+                  Back
+                </Button>
+              </Link>
+            </Card>
           </Card>
-        </Card>
         </Col>
       </Row>
     </Card>
