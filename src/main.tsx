dcd9112c6db9e8eeed23d36de62de77647ff798a
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-09-03 10:56:56 +0700

    added tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
+        <DevSupport
+            ComponentPreviews={ComponentPreviews}
+            useInitialHook={useInitial}
         >
-            <App/>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </DevSupport>
-    </React.StrictMode>,
-)
+    </React.StrictMode>
+);
