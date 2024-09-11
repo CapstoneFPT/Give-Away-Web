f37dcf59f89d58981708582ef9846cbd80a8c0d9
Gowwre <56794916+Gowwre@users.noreply.github.com>
2024-09-11 16:46:59 +0700

    fix connection

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,9 +1,2 @@
-//  export const BASE_URL = "https://giveawayproject.jettonetto.org:8443";
-//  export const BASE_URL = "https://103.177.111.181:8081" 
- export const BASE_URL = "http://localhost:5039";
-// export const BASE_URL = "https://golden-thrush-measured.ngrok-free.app/api";
-// ,{
-//     headers :{
-//       "ngrok-skip-browser-warning": "6942"
-//     }
-//   }
+ export const BASE_URL = "https://giveawayproject.jettonetto.org:8443";
+//  export const BASE_URL = "http://localhost:5039";
