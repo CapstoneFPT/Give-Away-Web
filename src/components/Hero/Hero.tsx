651eed6af28ba9624ff665933e45d587bf97e32e
Wi <wiwinov152000@gmail.com>
2024-09-24 22:10:26 +0700

    upatehome

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Hero.css";
 import { Carousel } from "antd";
+import hero3 from "../Assets/snapedit_1727189929462.jpeg"
+import hero2 from "../Assets/snapedit_1727190425797.jpeg"
+
 const Hero: React.FC = () => {
   return (
     <div className="hero">
@@ -20,7 +23,7 @@ const Hero: React.FC = () => {
         <Carousel style={{ marginTop: "2%" }} autoplay={true}>
           <div >
             <img
-              style={{ width: "100%", height: "890px" }}
+              style={{ width: "100%", height: "1100px" }}
               
               src="https://www.elleman.vn/app/uploads/2022/03/03/212156/Phoi-do-voi-phong-cach-retro-mua-He-elle-man-cover.jpg "
               alt="Hero 1"
@@ -29,15 +32,15 @@ const Hero: React.FC = () => {
           </div>
           <div>
             <img
-              style={{ width: "100%", height: "890px" }}
-              src="https://cdn-images.vtv.vn/2022/1/13/photo-1-16420742504011699677621-crop-16420746973442116528313.jpg"
+              style={{ width: "100%", height: "1100px" }}
+              src={hero2}
               alt="Hero 2"
             />
           </div>
           <div>
             <img
-              style={{ width: "100%", height: "890px" }}
-              src="https://emoi.vn/wp-content/uploads/2020/11/do-secondhand-7.png"
+              style={{ width: "100%", height: "1100px" }}
+              src={hero3}
               alt="Hero 3"
             />
           </div>
